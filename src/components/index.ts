3ac9d77f3ca9e9889d96fbc913f45e8ee177537c
smayankthakur <smayankthakur25@gmail.com>
2026-04-22 15:11:37 +0530

    fix: remove incorrect barrel file

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// Components barrel - export all public components
-export { default as Button } from './Button/Button';
-export { default as Card } from './Card/Card';
-export { default as Input } from './ui/Input';
-export { default as FloatingInput } from './ui/FloatingInput';
-export { default as FloatingTextarea } from './ui/FloatingTextarea';
